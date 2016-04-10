1006071a287c3d28d51300521495be575e754f91
Miguel Á. Domínguez Coloma <m@eridem.net>
2016-04-10 11:30:53 +0100

    Remove unecessary files

--- dist/js/ts/dependencies.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-var should = chai.should();
-module.exports = should;
--- dist/js/ts/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-var Patata = require('./patata');
-module.exports = new Patata.Patata();
