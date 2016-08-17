22663821e04880e7982425b401a58130dc9ffd72
Miguel Ángel Domínguez Coloma <miguel.dominguez@betssongroup.com>
2016-08-17 11:19:51 +0200

    Fix bug on reports

--- dist/cucumber/support/reports.js
+++ dist/cucumber/support/reports.js
@@ -3,7 +3,7 @@ const path = require('path');
 let reporter = function () {
     let reportHelper = require(path.join(__dirname, '../../index')).reportHelper;
     let reports = require(path.join(__dirname, '../../index')).reports;
-    this.chainReportPromises = function (reports, event, fnString, callback) {
+    let chainReportPromises = function (reports, event, fnString, callback) {
         reports = reports || [];
         // First dummy chain
         var promise = new Promise((resolve) => { resolve(); });
@@ -16,34 +16,39 @@ let reporter = function () {
                 return report[fnString](event) || new Promise((resolve) => { resolve(); });
             });
         });
-        return promise.then(callback);
+        Promise.all([promise]).then(() => {
+            callback();
+        }).catch(reason => {
+            console.log(`[Patata] Error on reports: ${JSON.stringify(reason)}`);
+            callback();
+        });
     };
-    this.registerHandler('BeforeFeature', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toFeature(event), 'beforeFeature', callback);
+    this.registerHandler('BeforeFeature', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toFeature(event), 'beforeFeature', callback);
     });
-    this.registerHandler('AfterFeature', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toFeature(event), 'afterFeature', callback);
+    this.registerHandler('AfterFeature', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toFeature(event), 'afterFeature', callback);
     });
-    this.registerHandler('FeaturesResult', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toFeaturesResult(event), 'featuresResult', callback);
+    this.registerHandler('FeaturesResult', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toFeaturesResult(event), 'featuresResult', callback);
     });
-    this.registerHandler('BeforeScenario', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toScenario(event), 'beforeScenario', callback);
+    this.registerHandler('BeforeScenario', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toScenario(event), 'beforeScenario', callback);
     });
-    this.registerHandler('AfterScenario', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toScenario(event), 'afterScenario', callback);
+    this.registerHandler('AfterScenario', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toScenario(event), 'afterScenario', callback);
     });
-    this.registerHandler('ScenarioResult', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toScenarioResult(event), 'scenarioResult', callback);
+    this.registerHandler('ScenarioResult', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toScenarioResult(event), 'scenarioResult', callback);
     });
-    this.registerHandler('BeforeStep', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toStep(event), 'beforeStep', callback);
+    this.registerHandler('BeforeStep', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toStep(event), 'beforeStep', callback);
     });
-    this.registerHandler('AfterStep', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toStep(event), 'afterStep', callback);
+    this.registerHandler('AfterStep', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toStep(event), 'afterStep', callback);
     });
-    this.registerHandler('StepResult', function (event, callback) {
-        return this.chainReportPromises(reports, reportHelper.toStepResult(event), 'stepResult', callback);
+    this.registerHandler('StepResult', (event, callback) => {
+        chainReportPromises(reports, reportHelper.toStepResult(event), 'stepResult', callback);
     });
 };
 module.exports = reporter;
